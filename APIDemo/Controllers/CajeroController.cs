c091cae8dc3ae1535b684007581c25ef02a82593
Sincal <sincaljulio200@gmail.com>
2024-03-22 20:37:03 -0600

    Creación Cajero Controller

---
+++ APIDemo/Controllers/CajeroController.cs
@@ -0,0 +1,70 @@
+﻿using APIDemo.models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace APIDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CajeroController : ControllerBase
+    {
+        public readonly string con;
+
+        public CajeroController(IConfiguration configuration)
+        {
+            con = configuration.GetConnectionString("conexion");
+        }
+
+        [HttpGet]
+        public IEnumerable<Cajero> Get()
+        {
+            List<Cajero> cajeros = new();
+
+            using (SqlConnection connection = new(con))
+            {
+                connection.Open();
+                using (SqlCommand cmd = new("sp_ObtenerCajeros", connection))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+
+                            Cajero cajero = new Cajero
+                            {
+                                CajeroID = reader.GetInt32(0),
+                                Biletes100 = reader.GetInt32(1),
+                                Billetes50 = reader.GetInt32(2),
+                            };
+
+                            cajeros.Add(cajero);
+
+                        }
+
+                    }
+                }
+            }
+            return cajeros;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Cajero c)
+        {
+            using (SqlConnection connection = new(con))
+            {
+                connection.Open();
+                using (SqlCommand cmd = new("sp_CrearCajero", connection))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@CajeroID", c.CajeroID);
+                    cmd.Parameters.AddWithValue("@Biletes100", c.Biletes100);
+                    cmd.Parameters.AddWithValue("@Billetes50", c.Billetes50);
+                    cmd.ExecuteNonQuery();
+
+                }
+            }
+        }
+    }
+}
---
+++ APIDemo/models/Cajero.cs
@@ -0,0 +1,9 @@
+﻿namespace APIDemo.models
+{
+    public class Cajero
+    {
+        public int CajeroID { get; set; }   
+        public int Biletes100 { get; set; }
+        public int Billetes50 { get; set; }
+    }
+}
